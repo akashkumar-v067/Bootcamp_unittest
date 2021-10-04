15d091a601dac9c80acfd72a2ce7b7d977fe77e5
akash kumar <akash.kumar2@tothenew.com>
2021-10-04 23:45:17 +0530

    test file created

---
+++ src/test/java/demo/domain/Ordertest.java
@@ -0,0 +1,11 @@
+package demo.domain;
+
+import org.junit.Test;
+
+public class Ordertest {
+    @Test
+    public void test1(){
+
+    }
+
+}
---
+++ src/test/java/demo/service/EmailServicetest.java
@@ -0,0 +1,10 @@
+package demo.service;
+
+import org.junit.Test;
+
+public class EmailServicetest {
+    @Test
+    public void test1(){
+
+    }
+}
